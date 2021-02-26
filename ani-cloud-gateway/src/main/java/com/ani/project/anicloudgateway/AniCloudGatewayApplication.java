e06048c86cdd32a8b73bf6eb0afb952de96af473
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-02-26 10:23:07 +0530

    api-gateway : implementing api gateway

---
+++ ani-cloud-gateway/src/main/java/com/ani/project/anicloudgateway/AniCloudGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.ani.project.anicloudgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AniCloudGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AniCloudGatewayApplication.class, args);
+	}
+
+}
--- patient-microservice/src/main/java/com/ani/project/patientmicroservice/patient/repository/PatientRepository.java
+++ patient-microservice/src/main/java/com/ani/project/patientmicroservice/patient/repository/PatientRepository.java
@@ -4,4 +4,5 @@ import com.ani.project.patientmicroservice.patient.domain.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PatientRepository extends JpaRepository<Patient, Long> {
+
 }
